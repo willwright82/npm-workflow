c0fdf1981cb7dc546d90c9626d8ef0f8416a06a1
Will Wright <will@wright.is>
2016-01-20 12:43:15 +0000

    Font Awesome installed

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,5 @@ var $ = require('jquery');
 global.jQuery = global.$ = $;
 
 var bootstrap = require('bootstrap');
-//var font-awesome = require('font-awesome');
 
-alert("Hello World!");
+// alert("Hello World!");
